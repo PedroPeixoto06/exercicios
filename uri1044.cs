d4ae05b2fe8f3372bea960a1e8c8a7df5c5f6ae1
Pedro Peixoto <pedroroger2553@gmail.com>
2025-06-14 19:04:18 -0300

    Add files via upload

---
+++ uri1044.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int a, b;
+
+        string[] numeros = Console.ReadLine().Split(' ');
+        a = Convert.ToInt32(numeros[0]);
+        b = Convert.ToInt32(numeros[1]);
+
+        if(a%b == 0 || b%a == 0)
+        {
+            Console.WriteLine("Sao multiplos");
+        } else
+        {
+            Console.WriteLine("Nao sao multiplos");
+        }
+
+    }
+
+}
