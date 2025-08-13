15c06c75bb0ba0774c9cefa4aec0fb14f7bcdc68
Pedro Peixoto <pedroroger2553@gmail.com>
2025-08-13 18:44:22 -0300

    Add files via upload

---
+++ uri1046.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class URI
+{
+    static void Main(String[] args)
+    {
+        int hi, hf;
+        int duração = 0;
+
+        string[] vet = Console.ReadLine().Split(' ');
+
+        hi = int.Parse(vet[0]);
+        hf = int.Parse(vet[1]);
+
+        if (hi == hf)
+        {
+            duração = 24;
+        }
+        else if (hi > hf)
+        {
+            duração = 24 - hi + hf;
+        }
+        else if (hi < hf)
+        {
+            duração = hf - hi;
+        }
+        Console.WriteLine("O JOGO DUROU " + duração + " HORA(S)");
+    }
+    }
+
+
